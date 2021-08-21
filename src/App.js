ae2211a4a4eb5b47543a7713ac24873591f61de1
Kaustuv Karan <kaustuvkaran01@gmail.com>
2021-08-21 21:06:13 +0530

    chore: cleaned for deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg'
-import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Home from './pages/Home';
-import Landing from './pages/Landing'
+import Home from "./pages/Home";
+import Landing from "./pages/Landing";
 
 function App() {
-  // Write new routes by adding a route tag underneath the home route by doing something like this: 
-  // <Route path="<YOUR_PATH_URL>" component={<NAME_OF_COMPONENT>}/>
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Landing}/>
-          <Route path="/home" component={Home}/>
-        </Switch>
-      </Router>
-    </div>
-  );
+	// Write new routes by adding a route tag underneath the home route by doing something like this:
+	// <Route path="<YOUR_PATH_URL>" component={<NAME_OF_COMPONENT>}/>
+	return (
+		<div className="App">
+			<Router>
+				<Switch>
+					<Route exact path="/" component={Landing} />
+					<Route path="/home" component={Home} />
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
